f6de4733683347fcc59a16ec2f77cf11c3593dee
Roman <blackwanted@yandex.ru>
2021-02-07 21:22:37 +0300

    --Update

--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
 export * from "./src/WebServer.ts";
 export * from "./src/core/WS_Context.ts";
 export * from "./src/core/WS_DefaultClass.ts";
+export * from "./src/core/WS_Router.ts";
 export * from "./src/error/WS_Error.ts";
